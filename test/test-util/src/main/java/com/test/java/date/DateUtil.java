c1a52688ba2d4074b52340cc052c7fba1969825a
942391815 <942391815@qq.com>
2015-07-26 11:08:28 +0800

    date util

--- test/test-util/src/main/java/com/test/java/date/DateUtil.java
+++ test/test-util/src/main/java/com/test/java/date/DateUtil.java
@@ -1,7 +1,34 @@
 package com.test.java.date;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateUtil {
-	public String formatDate(){
-		return null;
+	public static String formatDate(Date date,String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try{
+			return sdf.format(date);
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public static String formatNowDate(String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try{
+			return sdf.format(new Date());
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
+	}
+	public static Date stringConvertDate(String date,String pattern){
+		SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+		try{
+			return sdf.parse(date);
+		}catch(Exception e){
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
