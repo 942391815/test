79a1b1d90556bbfc2ef0988f691857df1bea614b
micheal <942391815@qq.com>
2016-06-08 09:08:18 +0800

    删除无用class

--- test/test-mybaits/src/main/java/com/test/java/test/Test.java
+++ test/test-mybaits/src/main/java/com/test/java/test/Test.java
@@ -1,5 +1,6 @@
 package com.test.java.test;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,11 +16,11 @@ public class Test {
 		SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder()
 				.build(Resources.getResourceAsReader("config.xml"));
 		SqlSession session = sessionFactory.openSession();
-		Map map = new HashMap();
-		map.put("queryStart", 10);
-		map.put("queryEnd", 10);
-		List<Map> selectList = session.selectList("callProc",map);
-		for(Map each:selectList){
+		List pks = new ArrayList();
+		pks.add("002885c8a54e11e598bcfa163e84de1b");
+		pks.add("002d406b4b644759a2301f0498686621");
+		List<String> selectList = session.selectList("getMD5sByPks", pks);
+		for(String each:selectList){
 			System.out.println(each);
 		}
 	}
