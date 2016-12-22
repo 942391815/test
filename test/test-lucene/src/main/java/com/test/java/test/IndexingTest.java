37263f40c96d1fce5e84b193ab954a974543fe30
qiaogu <qiaogu@jd.com>
2016-12-22 14:21:58 +0800

    test corebar

--- test/test-lucene/src/main/java/com/test/java/test/IndexingTest.java
+++ test/test-lucene/src/main/java/com/test/java/test/IndexingTest.java
@@ -13,11 +13,59 @@ import org.apache.lucene.search.*;
 import org.apache.lucene.store.RAMDirectory;
 import org.apache.lucene.util.BytesRef;
 
+import java.io.IOException;
+
 /**
  * Created by qiaogu on 2016/12/11.
  */
 public class IndexingTest {
     public static void main(String[] args) throws Exception{
+        testBooleanQuery();
+    }
+
+    public static void testBooleanQuery() throws Exception{
+        RAMDirectory directory = getRamDirectory();
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(reader);
+        NumericRangeQuery<Integer> ageQuery = NumericRangeQuery.newIntRange("age", 1, 20, true, true);
+        TermQuery termQuery = new TermQuery(new Term("name", "name20"));
+
+        BooleanQuery.Builder builder = new BooleanQuery.Builder();
+        builder.add(ageQuery,BooleanClause.Occur.MUST);
+        builder.add(termQuery,BooleanClause.Occur.MUST_NOT);
+
+        TopDocs search = indexSearcher.search(builder.build(), 100);
+        System.out.println(search.totalHits);
+
+    }
+    public static void testProfixQuery() throws Exception{
+
+        RAMDirectory directory = getRamDirectory();
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(reader);
+        PrefixQuery prefixQuery = new PrefixQuery(new Term("name", "name1"));
+        TopDocs search = indexSearcher.search(prefixQuery, 100);
+        System.out.println(search.totalHits);
+
+        TopDocs search1 = indexSearcher.search(new TermQuery(new Term("name", "name1")), 100);
+        System.out.println(search1.totalHits);
+    }
+
+    public static void testTermQuery() throws Exception{
+        RAMDirectory directory = getRamDirectory();
+
+        DirectoryReader reader = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(reader);
+        TermRangeQuery termRangeQuery = new TermRangeQuery("name", new BytesRef("name1".getBytes()), new BytesRef("name7".getBytes()), true, true);
+
+        TopDocs search = indexSearcher.search(termRangeQuery, 100);
+        for (int i=0;i<search.totalHits;i++){
+            Document doc = indexSearcher.doc(i);
+            System.out.println(doc.get("name"));
+        }
+    }
+
+    private static  RAMDirectory getRamDirectory() throws IOException {
         RAMDirectory directory = new RAMDirectory();
         StandardAnalyzer standardAnalyzer = new StandardAnalyzer();
         IndexWriterConfig config = new IndexWriterConfig(standardAnalyzer);
@@ -30,6 +78,11 @@ public class IndexingTest {
             indexWriter.addDocument(document);
         }
         indexWriter.close();
+        return directory;
+    }
+
+    public static void testNumberRangeQuery() throws Exception{
+        RAMDirectory directory = getRamDirectory();
 
         DirectoryReader reader = DirectoryReader.open(directory);
         IndexSearcher indexSearcher = new IndexSearcher(reader);
