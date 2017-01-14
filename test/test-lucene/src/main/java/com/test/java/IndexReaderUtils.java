651776da5f47816de011481e6e19069e4adac48c
qiaogu <2011578707@qq.com>
2017-01-14 21:42:45 +0800

    test

--- test/test-lucene/src/main/java/com/test/java/IndexReaderUtils.java
+++ test/test-lucene/src/main/java/com/test/java/IndexReaderUtils.java
@@ -1,18 +1,18 @@
-//package com.test.java;
-//
-//
-//import org.apache.lucene.analysis.Analyzer;
-//import org.apache.lucene.index.DirectoryReader;
-//import org.apache.lucene.index.IndexReader;
-//import org.apache.lucene.index.IndexWriter;
-//import org.apache.lucene.index.IndexWriterConfig;
-//import org.apache.lucene.store.Directory;
-//
-///**
-// * Created by qiaogu on 2016/12/11.
-// */
-//public class IndexReaderUtils {
-//    public static IndexReader getIndexWriter(Directory directory) throws Exception {
-//        return DirectoryReader.open(directory);
-//    }
-//}
+package com.test.java;
+
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+
+/**
+ * Created by qiaogu on 2016/12/11.
+ */
+public class IndexReaderUtils {
+    public static IndexReader getIndexReader(Directory directory) throws Exception {
+        return DirectoryReader.open(directory);
+    }
+}
