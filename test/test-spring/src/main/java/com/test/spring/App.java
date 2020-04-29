f8824adfb63c0158d6a03133dc32caef43f12f3c
qiaogu <qiaogu@xiaomi.com>
2020-04-29 19:53:50 +0800

    test spring-boot

---
+++ test/test-spring/src/main/java/com/test/spring/App.java
@@ -0,0 +1,20 @@
+package com.test.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class App {
+    @RequestMapping("/")
+    public String home(){
+        return "hello world!!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        SpringApplication.run(App.class,args);
+    }
+}
